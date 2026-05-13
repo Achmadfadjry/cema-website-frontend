d2fdeda8d793bc35ce1f7a81c8f5371353b3d715
NichoIrawan <Nicholas2815@hotmail.com>
2026-05-13 11:32:52 +0700

    Edit next config to rewrite API path

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,8 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: "/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
+        source: "/api/:path*",
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
       },
     ];
   },
