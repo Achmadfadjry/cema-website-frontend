685b13f9944875be99962d392af8de002d653fea
NichoIrawan <Nicholas2815@hotmail.com>
2026-06-11 02:03:47 +0700

    Add maze snippet

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { NextAuthProvider } from "@/components/NextAuthProvider";
+import Script from "next/script"; // <-- 1. Import Script dari next/script
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,6 +28,43 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* 2. Tambahkan script Maze di dalam tag head */}
+        <Script
+          id="maze-universal-tracking"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function (m, a, z, e) {
+                var s, t, u, v;
+                try {
+                  t = m.sessionStorage.getItem('maze-us');
+                } catch (err) {}
+              
+                if (!t) {
+                  t = new Date().getTime();
+                  try {
+                    m.sessionStorage.setItem('maze-us', t);
+                  } catch (err) {}
+                }
+              
+                u = document.currentScript || (function () {
+                  var w = document.getElementsByTagName('script');
+                  return w[w.length - 1];
+                })();
+                v = u && u.nonce;
+              
+                s = a.createElement('script');
+                s.src = z + '?apiKey=' + e;
+                s.async = true;
+                if (v) s.setAttribute('nonce', v);
+                a.getElementsByTagName('head')[0].appendChild(s);
+                m.mazeUniversalSnippetApiKey = e;
+              })(window, document, 'https://snippet.maze.co/maze-universal-loader.js', '8ab9ce70-95b1-4f80-8a2e-8efb336bb9b6');
+            `,
+          }}
+        />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <AuthProvider>
           {children}
